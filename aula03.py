b6b47dfb95366dea4fd6bf7b91ad377c11e9983d
Leandro Ramos Cardoso <lrcardoso.jp@gmail.com>
2024-12-15 10:36:45 -0300

    151224

---
+++ aula03.py
@@ -0,0 +1,23 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte (Python ja sabe o tipo sem precisarmos informar)
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Manuela')
+
+# Aspas duplas
+print("Kézya Medeiros")
+
+# Escape
+print("Kézya \"Medeiros\"")
+
+# r -> Exibe o caractere de escape
+print(r"Kézya \"Medeiros\"")
+
+# Outra maneira de exibir o caractere
+print(1, '"Kézya "Medeiros"')
+print(2, "Kézya 'Medeiros'")
---
+++ aula08.py
@@ -0,0 +1,15 @@
+# Exercicio 01 (Variaveis)
+
+nome = 'Manuela'
+sobrenome = 'Medeiros Cardoso'
+idade = 2
+ano_nascimento = 2022
+maior_de_idade = 2
+altura_metros = 0.60
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade >= 18)
+print('Altura em metros:', altura_metros)
